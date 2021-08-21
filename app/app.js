fa69a860c01b678cb999b991aae34200ebbd9b58
CuerpoCasa <gabialberini733@gmail.com>
2021-08-21 14:42:13 -0300

    v2.0 grid aplicada, no centrada

--- app/app.js
+++
@@ -1,8 +0,0 @@
-let temperatura = prompt("¿Cuantos años tiene tu mascota?");
-
-function convertidor(temperatura) {
-    let edadHumana = (temperatura * 7);
-    return edadHumana;
-}
-
-alert(convertidor(temperatura) + " ¿Muy interesante no? ¿Ya lo sabias?" )
