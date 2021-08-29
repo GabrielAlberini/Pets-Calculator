047341235c3db728aa96a4e23a9ad1ab394bb0b4
CuerpoCasa <gabialberini733@gmail.com>
2021-08-28 23:54:30 -0300

    V3.0 Version 3

--- app/app.js
+++ app/app.js
@@ -6,3 +6,14 @@ function convertidor(temperatura) {
 }
 
 alert(convertidor(temperatura) + " ¿Muy interesante no? ¿Ya lo sabias?" )
+
+
+
+
+
+let nombre = prompt("¿Como se llama tu mascota?");
+
+function petsType(type) { 
+    let dogOrCat = prompt("Tu mascota, es perro o gato"); 
+    return("Tu mascota es " + dogOrCat) 
+}
